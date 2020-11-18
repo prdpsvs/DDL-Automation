f9e64052c6b231fe09166fe4eda658daa7f5d933
Pradeep Venkata Satya Srikakolapu <pvenkat@microsoft.com>
2020-11-18 15:42:04 +0530

    starting from first

--- Athena-DDLAutomationDemo/Athena-DDLAutomation/ATHENA_DW/Views/CHANNEL_DIM_VW.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE VIEW [ATHENA_DW].[CHANNEL_DIM_VW]
-AS SELECT CHANNEL_ID                      
-     , CHANNEL_CODE                    
-     , CHANNEL_DESC                    
-     , CREATE_AUDIT_ID                 
-     , CREATE_DB_TIMESTAMP             
-     , UPDATE_AUDIT_ID                 
-     , UPDATE_DB_TIMESTAMP             
-  FROM ATHENA_DW.CHANNEL_DIM;
